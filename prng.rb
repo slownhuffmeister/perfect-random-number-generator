59786253ff3727920169d0c27a7188a23adfec19
Slown Huffmeister <i4evaSloown@aol.com>
2012-12-21 23:09:50 -0500

    greatness created

--- prng.rb
+++ prng.rb
@@ -1,18 +1,26 @@
-require 'uri'
 require 'net/http'
 
-module RandomOrg
-	class Integer
-		def initialize(min, max)
-			begin
-				res = Net::HTTP.get('www.random.org', "/integers/?num=1&min=#{min}&max=#{max}&col=1&base=10&format=plain&rnd=new").chomp
-				@value = res.to_i
-			rescue Net::HTTPFatalError => e
-				puts "Error: " + e
-			end
-		end
-		def to_i
-			@value
+module PerfectRandom
+	extend self
+	M = 2**32
+	A = 1664525
+	C = 1013904223
+	@cur_val = 0
+
+	def seed min=0, max=100000
+		begin
+			@cur_val  = Net::HTTP.get('www.random.org', "/integers/?num=1&min=#{min}&max=#{max}&col=1&base=10&format=plain&rnd=new").chomp.to_i
+		rescue Net::HTTPFatalError => e
+			puts e
 		end
 	end
+
+	def rand 
+		@cur_val = (A*@cur_val + C)%M
+	end
 end
+
+PerfectRandom::seed
+puts PerfectRandom::rand
+puts PerfectRandom::rand
+puts PerfectRandom::rand
