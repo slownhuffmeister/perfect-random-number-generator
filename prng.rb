b50827ecb2060c2823a17d0de3ad9e66f7d6711b
Slown Huffmeister <i4evaSloown@aol.com>
2012-12-22 00:43:41 -0500

    fixed it so you can't reseed. That would be a bug not a feature

--- prng.rb
+++ prng.rb
@@ -5,22 +5,24 @@ module PerfectRandom
 	M = 2**32
 	A = 1664525
 	C = 1013904223
-	@cur_val = 0
+	@val = 0
+	@seeded = false
 
 	def seed min=0, max=100000
 		begin
-			@cur_val  = Net::HTTP.get('www.random.org', "/integers/?num=1&min=#{min}&max=#{max}&col=1&base=10&format=plain&rnd=new").chomp.to_i
+			@val  = Net::HTTP.get('www.random.org', "/integers/?num=1&min=#{min}&max=#{max}&col=1&base=10&format=plain&rnd=new").chomp.to_i
 		rescue Net::HTTPFatalError => e
 			puts e
 		end
 	end
 
 	def rand 
-		@cur_val = (A*@cur_val + C)%M
+		seed if not @seeded
+			@seeded = true
+		@val = (A*@val + C)%M
 	end
 end
 
-PerfectRandom::seed
 puts PerfectRandom::rand
 puts PerfectRandom::rand
 puts PerfectRandom::rand
